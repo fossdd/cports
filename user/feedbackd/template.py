09eab42753cad76d4fb8cddd28da1d857d4492a9
fossdd <fossdd@pwned.life>
2025-03-22 03:33:21 +0100

    user/feedbackd: new package

---
+++ user/feedbackd/template.py
@@ -0,0 +1,39 @@
+pkgname = "feedbackd"
+pkgver = "0.8.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "glib-devel",
+    "meson",
+    "pkgconf",
+    "gobject-introspection",
+    "vala",
+]
+makedepends = [
+    "json-glib-devel",
+    "gobject-introspection-devel",
+    "gmobile-devel",
+    "gsound-devel",
+    "libgudev-devel",
+    "umockdev-devel",
+]
+depends = ["dbus"]
+checkdepends = ["dbus"]
+pkgdesc = "Daemon to provide haptic, LED, and audio feedback"
+license = "GPL-3.0-or-later"
+url = "https://source.puri.sm/Librem5/feedbackd"
+source = (
+    f"https://sources.phosh.mobi/releases/feedbackd/feedbackd-{pkgver}.tar.xz"
+)
+sha256 = "20c22656a89a207b592dc5c91a61285745e39b19248993d9f823fa7a834b8d9e"
+# i dont care
+options = ["!check"]
+
+
+def post_install(self):
+    self.uninstall("usr/lib/systemd/user")
+
+
+@subpackage("feedbackd-devel")
+def _(self):
+    return self.default_devel()
